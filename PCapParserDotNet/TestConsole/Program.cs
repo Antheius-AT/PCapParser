28aa242417f779cbfc14e843f93a5cc3ae955baa
Antheius <cs22m023@technikum-wien.at>
2023-05-17 19:04:02 +0200

    User controls eingebunden

--- PCapParserDotNet/TestConsole/Program.cs
+++ PCapParserDotNet/TestConsole/Program.cs
@@ -7,40 +7,68 @@ Console.SetBufferSize(Console.WindowWidth, Console.WindowHeight);
 var parser = new PCapParser();
 var maliciousUrls = new List<string>()
 {
-    "akamaiedge.net",
+    "tryhackme",
+    "google"
 };
 
-await parser.Parse(@"F:\FH_Technikum_Wien\2_Se\Firewalls\PcapFiles\Dump1.pcap");
+await parser.Parse(@"C:\Users\Grego\Downloads\wireshark\dump2.pcap");
 
 var maliciousMatches = parser.ResolvedDNSQueries.Where(p => maliciousUrls.Any(t => p.Contains(t)));
 
-var window = new Window(2, 2, Console.WindowHeight - 5, Console.WindowWidth - 5, 1, "DNQ queries");
+var window = new Window();
+window.ConsoleCoordinate.Left = 1;
+window.ConsoleCoordinate.Top = 1;
+window.Height = Console.WindowHeight - 5;
+window.Width = Console.WindowWidth - 5;
+window.Border.TopCharacter = '-';
+window.Border.BottomCharacter = '-';
 
-var listView = new ListView<TextBlock>(Console.WindowWidth - 10, Console.WindowHeight - 10, new ConsoleCoordinates(5, 5));
-listView.Content = parser.ResolvedDNSQueries.Select(p => new ListViewItem<TextBlock>(new TextBlock()
-{
-    Height = 1,
-    Width = 30,
-    Text = p,
-    InnerArea = maliciousMatches.Any(v => v == p)
-    ? new InnerArea(ConsoleColor.DarkRed, ConsoleColor.Yellow)
-    : new InnerArea(ConsoleColor.Black, ConsoleColor.White)
-})).ToList();
 
-window.AddChildren(listView);
-window.Show();
-//listView.Show();
+var listView = new ListView();
+listView.Width = Console.WindowWidth - 10;
+listView.Height = Console.WindowHeight - 10;
+listView.ConsoleCoordinate = new ConsoleCoordinate();
+listView.ConsoleCoordinate.Left = 5;
+listView.ConsoleCoordinate.Top = 5;
+
 Console.ReadKey(true);
 
-//Console.WriteLine("Tuples:");
-//foreach (var item in parser.TcpSessions)
-//{
-//    Console.WriteLine($"Source: {item.SrcIP}:{item.SourcePort}\nDestination: {item.DestIP}:{item.DestinationPort}");
-//}
+VisualizeDNSQueries();
 
-//foreach (var item in maliciousMatches)
-//{
-//    Console.WriteLine($"Malicious url found in pcap file {item}");
-//}
+Console.ReadKey(true);
 
-//Console.ReadKey(true);
+VisualizeTCPConnetions();
+
+Console.ReadKey(true);
+
+void VisualizeDNSQueries()
+{
+    listView.Content = parser.ResolvedDNSQueries.Distinct().Select(p => new TextBlock(p)
+    {
+        Height = 1,
+        Width = 30,
+        TrimText = true,
+        InnerArea = maliciousMatches.Any(v => v == p)
+    ? new ComponentInnerArea { ForegroundColor = ConsoleColor.DarkRed, BackgroundColor = ConsoleColor.Yellow }
+    : new ComponentInnerArea { ForegroundColor = ConsoleColor.Black, BackgroundColor = ConsoleColor.White }
+    }).ToList();
+
+
+    window.AddChildren(listView);
+
+    window.Draw();
+    window.DrawChildren();
+}
+
+void VisualizeTCPConnetions()
+{
+    listView.Content = parser.TcpSessions.Take(22).Select(p => new TextBlock($"TCP flow von {p.SrcIP}:{p.SourcePort} zu {p.DestIP}:{p.DestinationPort}. Insgesamt {p.TotalBytesTransferred} bytes geschickt")
+    {
+        Height = 1,
+        Width = Console.WindowWidth - 10,
+        TrimText = false,
+    }).ToList();
+
+    window.Draw();
+    window.DrawChildren();
+}
